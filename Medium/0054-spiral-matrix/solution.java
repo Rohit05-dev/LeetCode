a63d8715a17d38a4ace80db810833fd64f0f26da
Rohit05-dev <rohit05.dev@gmail.com>
2026-08-02 23:08:24 +0530

    LeetCode Problem No 189 Updated

---
+++ Medium/0054-spiral-matrix/solution.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class solution {
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
--- Medium/0189-rotate-array/solution.java
+++ Medium/0189-rotate-array/solution.java
@@ -1,13 +1,35 @@
 class solution {
+    /*
     public void rotate(int[] nums, int k) {
        int i = 0;
        int[] newArr = new int[nums.length];
        while (i < nums.length) {
-        newArr[i] = nums[(i+k)%nums.length];
+        newArr[(i+k)%nums.length] = nums[i];
+        i++;
        }
        for (int x = 0; x < nums.length; x++) {
             nums[x] = newArr[x];
         }
     }
+    */
     
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+
+    public void reverse(int[] nums, int left, int right) {
+        int temp = 0;
+        while (left < right) {
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
